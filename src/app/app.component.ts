20b856e9d448235a6e5ce547886faa47ff7fa74c
TheLe0 <leob.tosin@hotmail.com>
2022-10-24 19:29:44 -0300

    Finish the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-ng';
+  tasks: Task[] = [];
+
+  ngOnInit(): void {
+    this.tasks.push({
+      id: 1,
+      name: "Primeira task",
+      isClosed: false
+    })
+  }
 }
