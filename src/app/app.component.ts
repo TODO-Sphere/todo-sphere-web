ea9c67ec86c9f9620326d86fe97b48a3845d6c1b
TheLe0 <leob.tosin@hotmail.com>
2022-10-25 20:25:49 -0300

    Create a Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Task } from '../models/task';
 
 @Component({
@@ -7,14 +8,28 @@ import { Task } from '../models/task';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  createTaskForm!: FormGroup;
   title = 'todo-ng';
   tasks: Task[] = [];
 
+  constructor(private fb: FormBuilder) {
+
+  }
+
   ngOnInit(): void {
+    this.createTaskForm = this.fb.group({
+      content: ['', [Validators.required]]
+    });
+
     this.tasks.push({
       id: 1,
       name: "Primeira task",
       isClosed: true
     })
+  }
+
+  createTask(): void {
+    console.log(this.createTaskForm.get('content')?.value);
   }
 }
