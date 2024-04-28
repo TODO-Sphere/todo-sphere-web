e131f8e76e3cd9919cc19b321de79361025f83a5
Leonardo B. Tosin <leob.tosin@hotmail.com>
2024-04-28 19:38:39 -0300

    chore: Keep on Api mode until Mongo mode be stable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  dataMode: "atlas",
+  dataMode: "api",
   atlasAppId: "todo-sphere-backend-hnxycbq",
   todoApiBaseUrl:
     "https://todo-sphere-api.thankfulfield-713254b9.brazilsouth.azurecontainerapps.io/api/v1/",
